eadfc2177bee84bcc4681134cc560098925e86de
mara936 <mariannewamiru@gmail.com>
2025-08-29 16:49:01 +0300

    Added a discount folder

---
+++ discount/disc.py
@@ -0,0 +1,17 @@
+def calculate_discount(price, discount_percent):
+    if discount_percent >= 20:
+     discount_amount = (discount_percent/100) * price
+     final_price = price - discount_amount
+     return final_price
+    else:
+       return price
+    
+price = float(input("Enter original price: "))
+discount_percent = float(input("Enter the discount percentage: "))
+
+final_price = calculate_discount(price, discount_percent)
+
+if discount_percent >=20:
+ print(f"Final price after{discount_percent}% discount: {final_price}")
+else:
+ print (f"No discount applied. Final price: {final_price}")       
---
+++ discount/readme.md
@@ -0,0 +1,11 @@
+# Functions and Conditonal Statements
+  The python script describes the specific task that is performed to obtain a discount and a final price
+
+## Basic Features 
+  Defining a function
+  Using conditional statements to obtain the final price
+  Calling the function
+
+## To run
+Clone the repository or copy the code
+Use a debugger to run the code and check for errors 
